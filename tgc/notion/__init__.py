19504133ffea8466f57d7c6ddfd625fbb68d2ee8
truegoodcraft <truegoodcraft@gmail.com>
2025-10-12 19:59:27 -0400

    Add Notion sources registry sync helper

--- tgc/notion/__init__.py
+++ tgc/notion/__init__.py
@@ -1,5 +1,6 @@
 """Notion access module package."""
 
 from .module import NotionAccessModule
+from .sources import sync_data_sources_registry
 
-__all__ = ["NotionAccessModule"]
+__all__ = ["NotionAccessModule", "sync_data_sources_registry"]
