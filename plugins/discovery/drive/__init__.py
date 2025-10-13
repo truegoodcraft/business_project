562d66c3845d4f96ccc8633c490202b519524936
truegoodcraft <truegoodcraft@gmail.com>
2025-10-13 15:20:27 -0400

    Add tolerant plugin imports and safe logging

---
+++ plugins/discovery/drive/__init__.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+_base_path = Path(__file__).resolve().parents[2]
+_module_path = _base_path / 'discovery.drive/__init__.py'
+__file__ = str(_module_path)
+with _module_path.open('r', encoding='utf-8') as _fh:
+    exec(compile(_fh.read(), __file__, 'exec'))
