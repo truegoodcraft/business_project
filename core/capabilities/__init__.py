5816fab7f911336853ce37f471ef2379f98d1a04
truegoodcraft <truegoodcraft@gmail.com>
2025-10-14 18:15:33 -0400

    Use shared capability registry singleton

--- core/capabilities/__init__.py
+++ core/capabilities/__init__.py
@@ -1,1 +1,7 @@
-# makes core.capabilities a package
+from __future__ import annotations
+from .registry import CapabilityRegistry
+
+# Single global registry instance for the whole process
+REGISTRY = CapabilityRegistry(plugin_api_version="2")
+
+__all__ = ["REGISTRY", "CapabilityRegistry"]
