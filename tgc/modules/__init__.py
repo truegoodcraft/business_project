a7dda8b445f39cdae2a2bae3ee54215faaf8d406
truegoodcraft <truegoodcraft@gmail.com>
2025-11-20 14:44:41 -0500

    chore: final public cleanup – remove legacy/dev-only files

--- tgc/modules/__init__.py
+++
@@ -1,9 +0,0 @@
-# SPDX-License-Identifier: AGPL-3.0-or-later
-"""Optional controller modules."""
-
-from .google_drive import DriveModuleConfig, GoogleDriveModule
-
-__all__ = [
-    "DriveModuleConfig",
-    "GoogleDriveModule",
-]
